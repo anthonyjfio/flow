46a11e6680eb8a9f7b37e2067710b314a8a1055d
Anthony <ajfiorani@gmail.com>
2015-11-22 19:15:04 -0500

    remove gzipping of images

--- tools/gzip.sh
+++ tools/gzip.sh
@@ -11,8 +11,3 @@ echo "CSS Done"
 echo "Gzipping JS …"
 "$(npm bin)/zopfli" -i 1000 dist/js/*.js
 echo "JS Done"
-
-echo "Gzipping Images …"
-echo "This is going to take a while, go grab a snack"
-"$(npm bin)/zopfli" -i 1000 dist/img/*
-echo "Images Done"
